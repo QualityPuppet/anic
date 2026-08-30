5aecfbcf8166f7bc3607ba5102976770a206c198
QualityPuppet <evelynnraby@gmail.com>
2026-08-30 19:35:27 +0100

    making subtree is a pain, I'm just going to make my own workflow

--- dist/Anate/src/types/RankingState.ts
+++
@@ -1,8 +0,0 @@
-import { MediaListCollection, type Media } from "./anilist/MediaListCollections";
-import type { RankNode } from "./RankNode";
-
-export interface IRankingState {
-    InitialCollection: MediaListCollection | null,
-    TreeData: RankNode[],
-    Username: string
-}
