3c46fa7c9d9915e0544df0a443644538b4625ada
QualityPuppet <evelynnraby@gmail.com>
2026-09-02 13:16:10 +0100

    prettier sweep

--- Anate/src/router/index.ts
+++ Anate/src/router/index.ts
@@ -1,28 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'ranking',
-      component: () => import('../views/RankingView.vue')
-    },
-    {
-      path: '/statistics',
-      name: 'statistics',
-      component: () => import('../views/StatisticsView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "ranking",
+            component: () => import("../views/RankingView.vue")
+        },
+        {
+            path: "/statistics",
+            name: "statistics",
+            component: () => import("../views/StatisticsView.vue")
+        },
+        {
+            path: "/about",
+            name: "about",
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/AboutView.vue")
+        }
+    ]
+});
 
-  ],
-})
-
-export default router
+export default router;
--- Anate/src/types/strategies/StrategyInterface.ts
+++ Anate/src/types/strategies/StrategyInterface.ts
@@ -2,13 +2,13 @@ import type { RankingStore } from "@/stores/rankings";
 import type { Media } from "../anilist/MediaListCollections";
 
 export interface SortingStrategy {
-    RankedStore: Media[] 
-    InitialCollection: Media[]
+    RankedStore: Media[];
+    InitialCollection: Media[];
     Current: SortItem;
     ComparisonMedia: Media;
     Store: RankingStore;
 }
 
 export interface SortItem {
-    media: Media
+    media: Media;
 }
